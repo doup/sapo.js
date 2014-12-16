c2d125fa0a8b406430309a25510cb9874fb2999d
doup <asier@illarra.com>
2014-12-16 01:54:24 +0100

    Beam color

--- src/presets/beam.js
+++ src/presets/beam.js
@@ -1,7 +1,8 @@
 function render(s, t) {
-    var freq = 15;
-    var amp  = 0.05;
-    var beam = 1.2;
+    var freq  = 40;
+    var amp   = 0.05;
+    var beam  = 1.2;
+    var color = [1.0, 1.0, 0.5, 1.0];
 
     // Simetry
     s = s * 2;
@@ -11,5 +12,5 @@ function render(s, t) {
     // Distort
     s = s + Math.sin(t * Math.PI * freq * 2) * (amp / 2);
 
-    return [s * beam, 1.0, 0.5, s * beam];
+    return [color[0] * s * beam, color[1], color[2], color[3] * s * beam];
 }
