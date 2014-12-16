920937fad9edb69d0175e74292afbc9d455c6d79
doup <asier@illarra.com>
2014-12-16 01:25:22 +0100

    Improved beam

--- src/presets/beam.js
+++ src/presets/beam.js
@@ -1,6 +1,15 @@
 function render(s, t) {
+    var freq = 15;
+    var amp  = 0.05;
+    var beam = 1.2;
+
+    // Simetry
     s = s * 2;
     s = s > 1 ? 2 - s : s;
+    s = s - (amp / 2);
 
-    return [s * 1.1, 1.0, 0.5, s * 1.1];
+    // Distort
+    s = s + Math.sin(t * Math.PI * freq * 2) * (amp / 2);
+
+    return [s * beam, 1.0, 0.5, s * beam];
 }
